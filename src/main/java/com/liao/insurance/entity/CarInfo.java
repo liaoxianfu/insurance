a4f11206332d05a4f66521d53f3423b6e5c28981
liaoxianfu <liaoxianfu555@outlook.com>
2019-04-10 17:37:59 +0800

    修改汽车信息表的id为自动增长

--- src/main/java/com/liao/insurance/entity/CarInfo.java
+++ src/main/java/com/liao/insurance/entity/CarInfo.java
@@ -1,6 +1,9 @@
 package com.liao.insurance.entity;
 
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -20,6 +23,8 @@ public class CarInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
