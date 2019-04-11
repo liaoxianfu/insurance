00127316f7f63cd2378ef4399a8949fa1e389c00
liaoxianfu <liaoxianfu555@outlook.com>
2019-04-11 09:21:10 +0800

    1、修改公司添加逻辑 2、修改状态码注释为javadoc

--- src/main/java/com/liao/insurance/codeInfo/CodeInfo.java
+++ src/main/java/com/liao/insurance/codeInfo/CodeInfo.java
@@ -1,10 +1,15 @@
 package com.liao.insurance.codeInfo;
 
 public class CodeInfo {
-    // 公司已经存在状态码
+    /**
+     * 公司已经存在状态码
+     */
     public static int COMPANY_EXITS = -1;
 
-    // 公司创建成功状态码
+
+    /**
+     * 公司创建成功状态码
+     */
     public static int COMPANY_CREATE_SUCCESS = 1;
 
 
--- src/main/java/com/liao/insurance/controller/CompanyController.java
+++ src/main/java/com/liao/insurance/controller/CompanyController.java
@@ -7,7 +7,6 @@ import com.liao.insurance.service.ICompanyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,39 +26,48 @@ import static com.liao.insurance.codeInfo.CodeInfo.*;
 @RequestMapping("/insurance/company")
 public class CompanyController {
 
-    @Autowired
-    private ICompanyService companyService;
+    private final ICompanyService companyService;
 
     private static Logger logger = LoggerFactory.getLogger(CompanyController.class);
 
+    @Autowired
+    public CompanyController(ICompanyService companyService) {
+        this.companyService = companyService;
+    }
+
     @PostMapping("/")
-    public Object AddCompany(Company company) {
+    public Object addCompany(Company company) {
         ModelMap model = new ModelMap();
         // 返回不同的状态码
         int code = companyService.addCompany(company);
 
         logger.debug(company.toString());
         if (code == COMPANY_CREATE_SUCCESS) {
-            logger.debug("添加成功 code：{}",COMPANY_CREATE_SUCCESS);
             model.addAttribute("info", "添加成功");
-        } else {
-            logger.debug("添加失败 code {}",code);
+        }
+        else if (code==COMPANY_EXITS){
+            model.addAttribute("info","公司已经存在");
+        }
+        else {
             model.addAttribute("info", "添加失败");
         }
+        logger.debug("状态码---->{}",code);
         model.addAttribute("code", code);
-        logger.debug("返回的信息 {}",model);
+        logger.debug("返回的信息----> {}", model);
         return JSON.toJSON(model);
     }
 
     @GetMapping("/")
-    public Object getAllCompany(){
+    public Object getAllCompany() {
         List<Company> companyList = companyService.getAllCompany();
         return JSON.toJSON(companyList);
     }
 
     @GetMapping("/{id}")
-    public Object getCompanyById(@PathVariable Integer id){
+    public Object getCompanyById(@PathVariable Integer id) {
         Company company = companyService.getById(id);
         return JSON.toJSON(company);
     }
+
+
 }
